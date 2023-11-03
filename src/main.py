67912aac3dbe1d1781ff857df5b5b9577f98ef24
Hugo Larochelle <55212841+KookyEddie@users.noreply.github.com>
2023-11-03 08:50:07 -0400

    test

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import time
 import psycopg2
 
 
+# jsbdojsdfbgsjbfpksfbkspfbvknsfvpksfnl[dfnvkpsfnvsfbvkand[lvdnvadsmvnas[dlv,a;dlmanpofjah iogjspgijnargvonirgirnmg,iamjcgpoijgmc,igmoiva]]]
 class Main:
     def __init__(self, host, token, tickets, t_max, t_min, database):
         """Initialize with environment variables and provided values."""
