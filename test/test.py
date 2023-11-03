36ee022eba90cdb3e4b5c3904aabfb965401ff85
Marc-Olivier Bisson <bissonmarcolivier@gmail.com>
2023-11-02 23:39:59 -0400

    correction du fichier test

--- test/test.py
+++ test/test.py
@@ -1,14 +1,17 @@
 import sys
-sys.path.append('../src')
-
 import src.main as main
+
+sys.path.append("../src")
+
 
 def test_import_main():
     try:
         assert "Main" in dir(main)
         print("Import successful.")
     except Exception as e:
+        print(e, flush=True)
         assert False, f"Import failed: {dir(main.Main)}"
+
 
 def test_host_variable():
     mockMain = main.Main(
@@ -17,10 +20,11 @@ def test_host_variable():
         tickets=2,
         t_max=30,
         t_min=18,
-        database="test-database"
+        database="test-database",
     )
 
     assert "test-host", f"{mockMain.HOST}"
+
 
 def test_token_variable():
     mockMain = main.Main(
@@ -29,10 +33,11 @@ def test_token_variable():
         tickets=2,
         t_max=30,
         t_min=18,
-        database="test-database"
+        database="test-database",
     )
 
-    assert "test-token", f"{mockMain.TOKEN}"    
+    assert "test-token", f"{mockMain.TOKEN}"
+
 
 def test_T_MAX_variable():
     mockMain = main.Main(
@@ -41,10 +46,11 @@ def test_T_MAX_variable():
         tickets=2,
         t_max=30,
         t_min=18,
-        database="test-database"
+        database="test-database",
     )
 
-    assert 30, f"{mockMain.T_MAX}"       
+    assert 30, f"{mockMain.T_MAX}"
+
 
 def test_T_MIN_variable():
     mockMain = main.Main(
@@ -53,7 +59,7 @@ def test_T_MIN_variable():
         tickets=2,
         t_max=30,
         t_min=18,
-        database="test-database"
+        database="test-database",
     )
 
-    assert 18, f"{mockMain.T_MIN}"       
+    assert 18, f"{mockMain.T_MIN}"
